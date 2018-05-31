f637d4b490fc26927e0ced5411351f74c9dd460f
Andrew Joyce <andrew.joyce@mastercard.com>
2018-05-31 14:12:23 +0100

    Initial Commit

---
+++ ATSSDK-Sample/ATSSDK-Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ATSSDK-Sample
+//
+//  Created by Joyce, Andrew on 31/05/2018.
+//  Copyright © 2018 Joyce, Andrew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ATSSDK-iOS/ATSSDK-iOS/ATSSDK_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ATSSDK_iOS.h
+//  ATSSDK-iOS
+//
+//  Created by Joyce, Andrew on 31/05/2018.
+//  Copyright © 2018 Joyce, Andrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATSSDK_iOS.
+FOUNDATION_EXPORT double ATSSDK_iOSVersionNumber;
+
+//! Project version string for ATSSDK_iOS.
+FOUNDATION_EXPORT const unsigned char ATSSDK_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATSSDK_iOS/PublicHeader.h>
+
+
